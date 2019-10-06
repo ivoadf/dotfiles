9b5c05c5592356550193975ea1ef978ce661b8fa
Ivo Fernandes <ivoadf@gmail.com>
2019-10-06 08:00:08 +0100

    screen layout shortcuts

---
+++ .screenlayout/both_screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --off --output LVDS-0 --mode 1600x900 --pos 0x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 1600x0 --rotate normal
---
+++ .screenlayout/hdmi_only.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --off --output LVDS-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal
